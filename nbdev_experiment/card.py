5b64e194c407dc36381a06526bbcc47ff8ce8613
Eitan Lees <eitanlees@gmail.com>
2022-09-20 15:29:47 -0400

    finishing tutorial

--- nbdev_experiment/card.py
+++ nbdev_experiment/card.py
@@ -4,10 +4,13 @@
 __all__ = ['suits', 'ranks', 'Card']
 
 # %% ../00_card.ipynb 3
+from fastcore.utils import *
+
+# %% ../00_card.ipynb 4
 suits = ['♠️','♥️','♣️','♦️']
 ranks = [None, 'A'] + [str(x) for x in range(2, 11)] + ['J', 'Q', 'K']
 
-# %% ../00_card.ipynb 12
+# %% ../00_card.ipynb 13
 class Card:
     "A playing card"
     def __init__(self, 
@@ -16,3 +19,15 @@ class Card:
         self.suit, self.rank = suit, rank
     def __str__(self): return f"{ranks[self.rank]}{suits[self.suit]}"
     __repr__ = __str__
+
+# %% ../00_card.ipynb 18
+@patch
+def __eq__(self:Card, other:Card): return (self.suit, self.rank) == (other.suit, other.rank)
+
+# %% ../00_card.ipynb 20
+@patch
+def __gt__(self:Card, other:Card): return (self.suit, self.rank) > (other.suit, other.rank)
+
+# %% ../00_card.ipynb 22
+@patch
+def __lt__(self:Card, other:Card): return (self.suit, self.rank) < (other.suit, other.rank)
